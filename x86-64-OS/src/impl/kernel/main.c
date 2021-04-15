713693d5c498965a5d6fadecb4a3791ae7c4620b
shariq-cmd <57193290+shariq-cmd@users.noreply.github.com>
2021-04-15 20:03:16 +0500

    Add files via upload

---
+++ x86-64-OS/src/impl/kernel/main.c
@@ -0,0 +1,28 @@
+#include "print.h"
+
+void kernel_main() {
+print_clear();
+print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+print_str("             ");
+print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_RED);
+print_str(" MUHAMMAD SHARIQ <--------> 282526 <--------> BSCS-9B \n\n\n");
+
+print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
+    print_str("               #        #    @        @    $$$$$$$$$$    **********\n");
+print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+    print_str("               #\\       #    @        @    $                 *\n");
+print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_BLACK);
+    print_str("               # \\      #    @        @    $                 *\n");
+print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+    print_str("               #  \\     #    @        @    $                 *\n");
+print_set_color(PRINT_COLOR_MAGENTA, PRINT_COLOR_BLACK);
+    print_str("               #   \\    #    @        @    $$$$$$$$$$        *\n");
+print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("               #    \\   #    @        @             $        *\n");
+print_set_color(PRINT_COLOR_PINK, PRINT_COLOR_BLACK);
+    print_str("               #     \\  #    @        @             $        *\n");
+print_set_color(PRINT_COLOR_LIGHT_RED, PRINT_COLOR_BLACK);
+    print_str("               #      \\ #    @        @             $        *\n");
+print_set_color(PRINT_COLOR_LIGHT_BLUE, PRINT_COLOR_BLACK);
+    print_str("               #        #    @@@@@@@@@@    $$$$$$$$$$        *\n");
+}
